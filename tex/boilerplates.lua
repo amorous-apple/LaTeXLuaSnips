b14034dd867f0a5356c1d88bd14a06d0419925ac
CongLong <thdragonmaster@gmail.com>
2024-05-13 13:32:47 +0200

    Improved formatting for boilerplate code to serve as a formatting standard.

--- tex/boilerplates.lua
+++ tex/boilerplates.lua
@@ -1,27 +1,28 @@
 return {
-s({trig="homework", dscr="A convenient boilerplate for math homework. Add/delete packages according to your personal needs.This boilerplate has two additional environments, 'ex' and 'sol' (excercise and solution respectively)"},
-  fmta(
-     [[
-     \documentclass[12pt]{article}
+	s(
+		{trig="homework", dscr="A convenient boilerplate for math homework. Add/delete packages according to your personal needs.This boilerplate has two additional environments, 'ex' and 'sol' (excercise and solution respectively)"
+		},
+		fmta(
+			[[
+				\documentclass[12pt]{article}
 
-\usepackage[margin=1in]{geometry} 
-\usepackage{amsmath,amsthm,amssymb}
+				\usepackage[margin=1in]{geometry} 
+				\usepackage{amsmath,amsthm,amssymb}
 
-\newenvironment{ex}[2][Exercise]{\begin{trivlist}
-\item[\hskip \labelsep {\bfseries #1}\hskip \labelsep {\bfseries #2.}]}{\end{trivlist}}
+				\newenvironment{ex}[2][Exercise]{\begin{trivlist}
+				\item[\hskip \labelsep {\bfseries #1}\hskip \labelsep {\bfseries #2.}]}{\end{trivlist}}
 
-\newenvironment{sol}[1][Solution]{\begin{trivlist}
-\item[\hskip \labelsep {\bfseries #1:}]}{\end{trivlist}}
+				\newenvironment{sol}[1][Solution]{\begin{trivlist}
+				\item[\hskip \labelsep {\bfseries #1:}]}{\end{trivlist}}
 
 
-\begin{document}
-\noindent <> \hfill {\Large <>} \hfill \today
-<>
-\end{document}
-     ]],
-     { i(1, "Name"),i(2, "Title of the homework"),i(0) }
-  )
-),
-
+				\begin{document}
+				\noindent <> \hfill {\Large <>} \hfill \today
+				<>
+				\end{document}
+			]],
+			{ i(1, "Name"),i(2, "Title of the homework"),i(0) }
+	  	)
+	),
 }
  
