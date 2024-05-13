a5eaf883bb00039dd6749b8f895c11f23a0912ff
NablaSquaredNull <andrew.wave01@outlook.com>
2024-05-13 18:58:48 +0200

    added variables to mathoperators 'code was broken in past commit'

--- tex/mathoperators.lua
+++ tex/mathoperators.lua
@@ -1,3 +1,40 @@
+-- Abbreviations used in this article and the LuaSnip docs
+    local ls = require("luasnip")
+    local s = ls.snippet
+    local sn = ls.snippet_node
+    local isn = ls.indent_snippet_node
+    local t = ls.text_node
+    local i = ls.insert_node
+    local f = ls.function_node
+    local c = ls.choice_node
+    local d = ls.dynamic_node
+    local r = ls.restore_node
+    local events = require("luasnip.util.events")
+    local ai = require("luasnip.nodes.absolute_indexer")
+    local extras = require("luasnip.extras")
+    local l = extras.lambda
+    local rep = extras.rep
+    local p = extras.partial
+    local m = extras.match
+    local n = extras.nonempty
+    local dl = extras.dynamic_lambda
+    local fmt = require("luasnip.extras.fmt").fmt
+    local fmta = require("luasnip.extras.fmt").fmta
+    local conds = require("luasnip.extras.expand_conditions")
+    local postfix = require("luasnip.extras.postfix").postfix
+    local types = require("luasnip.util.types")
+    local parse = require("luasnip.util.parser").parse_snippet
+    local ms = ls.multi_snippet
+    local k = require("luasnip.nodes.key_indexer").new_key
+
+
+--LATEX PACKAGE DEPENDENCIES: amssymb,amsmath 
+local in_mathzone = function()
+  -- The `in_mathzone` function requires the VimTeX plugin
+  return vim.fn['vimtex#syntax#in_mathzone']() == 1
+end
+
+
 return{
 	
 --MATH MODE ACCENTS (ESSENTIALLY EVERYTHING THAT GOES ABOVE)
@@ -130,6 +167,12 @@ s({trig="lra",snippetType= "autosnippet",dscr= "Left Right arrow",wordTrig = fal
     {
       t("\\Leftrightarrow"),
   },{condition = in_mathzone}),
+
+s({trig="perp",snippetType= "autosnippet",dscr= "perpendicular symbol",wordTrig = false},
+    {
+      t("\\perp"),
+  },{condition = in_mathzone}),
+
 
 --MATHEMATICAL FONTS (ONLY TO BE USED IN MATH MODE!) MAYBE MAKE THEM AUTOSNIPPETS?
 
