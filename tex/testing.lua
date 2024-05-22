14e2b60e58990db4b43292a889b8cd5827f47c0c
NablaSquaredNull <andrew.wave01@outlook.com>
2024-05-22 22:24:41 +0200

    fixed subindices snippets triggering outside math mode. Added 'nn' snippet.

--- tex/testing.lua
+++ tex/testing.lua
@@ -5,7 +5,6 @@ local in_mathzone = function()
 end
 -- Then pass the table `{condition = in_mathzone}` to any snippet you want to
 -- expand only in math contexts.
-
 return {
 -- Examples of Greek letter snippets, autotriggered for efficiency
 s({trig=":exp", snippetType="autosnippet"},
@@ -20,7 +19,8 @@ s({trig = "([^%a])mm", wordTrig = false, regTrig = true},
       f( function(_, snip) return snip.captures[1] end ),
       d(1, get_visual),
     }
-  )
+  ),
+{condition = in_mathzone}
 ),
 
 -- Add a 0 subscript when typing 00 after a letter. 
@@ -31,7 +31,8 @@ s({trig = '([%a%)%]%}])00', regTrig = true, wordTrig = false, snippetType="autos
       f( function(_, snip) return snip.captures[1] end ),
       t("0")
     }
-  )
+  ),{condition = in_mathzone}
+
 ),
 
 -- Add a 1 subscript when typing 11 after a letter. 
@@ -42,7 +43,8 @@ s({trig = '([%a%)%]%}])11', regTrig = true, wordTrig = false, snippetType="autos
       f( function(_, snip) return snip.captures[1] end ),
       t("1")
     }
-  )
+  ),{condition = in_mathzone}
+
 ),
 
 -- Add a 2 subscript when typing 22 after a letter. 
@@ -53,7 +55,8 @@ s({trig = '([%a%)%]%}])22', regTrig = true, wordTrig = false, snippetType="autos
       f( function(_, snip) return snip.captures[1] end ),
       t("2")
     }
-  )
+  ),{condition = in_mathzone}
+
 ),
 
 -- Add a 3 subscript when typing 33 after a letter. 
@@ -64,7 +67,8 @@ s({trig = '([%a%)%]%}])33', regTrig = true, wordTrig = false, snippetType="autos
       f( function(_, snip) return snip.captures[1] end ),
       t("3")
     }
-  )
+  ),{condition = in_mathzone}
+
 ),
 
 -- Add a x subscript when typing xx after a letter. 
@@ -75,7 +79,8 @@ s({trig = '([%a%)%]%}])xx', regTrig = true, wordTrig = false, snippetType="autos
       f( function(_, snip) return snip.captures[1] end ),
       t("x")
     }
-  )
+  ),{condition = in_mathzone}
+
 ),
 
 -- Add a y subscript when typing yy after a letter. 
@@ -86,7 +91,8 @@ s({trig = '([%a%)%]%}])yy', regTrig = true, wordTrig = false, snippetType="autos
       f( function(_, snip) return snip.captures[1] end ),
       t("y")
     }
-  )
+  ),{condition = in_mathzone}
+
 ),
 
 -- Add a z subscript when typing zz after a letter. 
@@ -97,7 +103,8 @@ s({trig = '([%a%)%]%}])zz', regTrig = true, wordTrig = false, snippetType="autos
       f( function(_, snip) return snip.captures[1] end ),
       t("z")
     }
-  )
+  ),{condition = in_mathzone}
+
 ),
 
 -- Add a m subscript when typing mm after a letter. 
@@ -108,7 +115,22 @@ s({trig = '([%a%)%]%}])mm', regTrig = true, wordTrig = false, snippetType="autos
       f( function(_, snip) return snip.captures[1] end ),
       t("m")
     }
-  )
+  ),{condition = in_mathzone}
+
 ),
+
+-- Add a n subscript when typing nn after a letter. 
+s({trig = '([%a%)%]%}])nn', regTrig = true, wordTrig = false, snippetType="autosnippet"},
+  fmta(
+    "<>_{<>}",
+    {
+      f( function(_, snip) return snip.captures[1] end ),
+      t("n")
+    }
+  ),{condition = in_mathzone}
+
+),
+
+
 
 }
